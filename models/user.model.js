4f5b3efb4ee546dcd334fdfc2bceb6925074b24f
josemak25 <amakirij@gmail.com>
2020-02-12 18:59:44 +0100

    add user model

---
+++ models/user.model.js
@@ -0,0 +1,60 @@
+import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true
+    },
+    avatar: {
+      type: String,
+      default: null
+    },
+    gender: {
+      type: String,
+      enum: ['male', 'female'],
+      default: null
+    },
+    phone: {
+      type: String,
+      minlength: 11,
+      maxlength: 11,
+      default: null
+    },
+    password: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
+    toObject: { virtuals: true }
+  }
+);
+
+/**
+ * pre-save hooks
+ */
+UserSchema.pre('save', () => {});
+
+/**
+ * Methods
+ */
+UserSchema.methods = {
+  toJSON() {
+    const { password, _id: id, __v, ...rest } = this.toObject();
+    return { ...rest, id };
+  }
+};
+
+/**
+ * Statics
+ */
+UserSchema.statics = {};
+
+export default model('user', UserSchema);
