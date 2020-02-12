00b1a97808fb62a086d7cd3d44f6bf68d0d1121d
josemak25 <amakirij@gmail.com>
2020-02-12 19:01:25 +0100

    add test for user model

---
+++ models/__tests__/user.model.test.js
@@ -0,0 +1,15 @@
+import User from '../user.model';
+
+describe('User model', () => {
+  test('email, name and password must be required', async () => {
+    expect.assertions(1);
+    try {
+      await User.create({
+        gender: 'male',
+        phone: '07019703051'
+      });
+    } catch (e) {
+      expect(e).toBeTruthy();
+    }
+  });
+});
